d69baeb3997d420c19939e3df6ae3f67f3862700
Naman Jain <worknj15@gmail.com>
2021-10-06 23:58:42 +0530

    Created a structure and thought of navigation

--- app/src/main/java/com/example/watchlist/ui/MainActivity.kt
+++ app/src/main/java/com/example/watchlist/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.watchlist
+package com.example.watchlist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.watchlist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/watchlist/ui/splashfragment/SplashFragment.kt
@@ -0,0 +1,16 @@
+package com.example.watchlist.ui.splashfragment
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.example.watchlist.R
+
+class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
